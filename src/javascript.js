b9654baf50629d9c31ef0905299b2ac70cd817b1
DiegoSanZar <81200983+DiegoSanZar@users.noreply.github.com>
2021-06-24 20:57:30 -0500

    Delete package-lock.json

--- src/javascript.js
+++
@@ -1,7 +0,0 @@
-
-document.getElementsByClassName(".nav a").onclick =  function(e){
-
-    document.getElementsByClassName(".nav a").find(".active").removeClass("active");
-    document.getElementsByClassName(".nav a").parent().addClass("active");
-  }
-
